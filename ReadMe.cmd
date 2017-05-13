0721df69986993a20960f6a2f238214b018d4584
dnl.riccio <dnl.riccio@gmail.com>
2017-05-13 04:26:52 +0200

    Loading files

---
+++ ReadMe.cmd
@@ -0,0 +1,12 @@
+
+Using direction and score information for retina based person verification
+
+--------------------------------------------------------------------------
+
+Biometric systems based on retinal image analysis are of interest for highly accurate person verification. In this framework, we present a person verification method involving retina segmentation, co-registration, feature extraction and matching. All processes are based on the construction and use of a direction map and of an associated score map. An interesting feature of the method is the possibility to accomplish satisfactorily person verification also when the retinal image is not fully available at high quality, as it could be the case due to aging or retinal diseases of the subject. The proposed approach has been tested on publicly available datasets and compared with other verification methods in the literature. The experimental results confirm the goodness of the proposed approach and show that direction and score maps have a key role in person verification.
+
+
+-------------------------------------------------------
+
+Matlab Implementation by Daniel Riccio, May 12, 2017.
+Copyright (C) 2017 Daniel Riccio (dnl.riccio@gmail.com)
